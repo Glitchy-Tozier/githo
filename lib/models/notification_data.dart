c64b298b9bc2ac71f4ae849a6e93252acf369d95
Glitchy-Tozier <f.thaler@posteo.net>
2022-02-05 18:06:27 +0100

    Add NotificationData - class

---
+++ lib/models/notification_data.dart
@@ -0,0 +1,56 @@
+/* 
+ * Githo – An app that helps you gradually form long-lasting habits.
+ * Copyright (C) 2021 Florian Thaler
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import 'package:githo/helpers/type_extentions.dart';
+
+/// A model for how and when notifications should be displayed.
+
+class NotificationData {
+  NotificationData({
+    required this.isActive,
+    required this.nextActivationDate,
+    required this.hoursBetweenNotifications,
+  });
+
+  /// Converts a Map into [NotificationData].
+  NotificationData.fromMap(final Map<String, dynamic> map)
+      : isActive = (map['isActive'] as int).toBool(),
+        nextActivationDate =
+            DateTime.parse(map['nextActivationDate'] as String),
+        hoursBetweenNotifications = map['hoursBetweenNotifications'] as int;
+
+  /// Supplies the default instance of [NotificationData].
+  NotificationData.initialValues()
+      : isActive = false,
+        nextActivationDate = DateTime.now(),
+        hoursBetweenNotifications = 9999;
+
+  bool isActive;
+  DateTime nextActivationDate;
+  int hoursBetweenNotifications;
+
+  /// Converts the [NotificationData] into a Map.
+  Map<String, dynamic> toMap() {
+    final Map<String, dynamic> map = <String, dynamic>{
+      'isActive': isActive.toInt(),
+      'nextActivationDate': nextActivationDate.toString(),
+      'hoursBetweenNotifications': hoursBetweenNotifications,
+    };
+    return map;
+  }
+}
